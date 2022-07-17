05eb95b5ddee4a6546b3b4ab7b8be8571eb7a865
exd02 <eliamxd1337@gmail.com>
2022-07-17 14:22:14 -0300

    added preloader

--- 9-Video/main.js
+++ 9-Video/main.js
@@ -1,5 +1,6 @@
 const button = document.querySelector('.switch-options')
 const video = document.querySelector('.video-container')
+video.volume = 0;
 
 function pauseVideo() {
   if (button.classList.contains('off')) {
@@ -10,3 +11,9 @@ function pauseVideo() {
     button.classList.add('off')
   }
 }
+
+/* PRELOADER */
+const preloader = document.querySelector(".preloader");
+window.addEventListener("load", function () {
+  preloader.classList.add("hide-preloader");
+});
