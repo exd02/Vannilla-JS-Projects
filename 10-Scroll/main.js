74e34271b78d77f61e4ad27449bfd7f978c77a77
exd02 <eliamxd1337@gmail.com>
2022-07-18 18:36:07 -0300

    adjusted the low-res container link to auto-get the height of the links

--- 10-Scroll/main.js
+++ 10-Scroll/main.js
@@ -1,7 +1,25 @@
-/* - MENU NAVIGATION - */
+
+const navLinks = document.querySelectorAll('.nav-links')
 navRight = document.querySelector('.nav-right')
+
+console.log (navRight.style.height)
+
+
+/* - MOBILE NAVIGATION - */
 function toggleMenuView() {
-  navRight.classList.toggle('show')
+  if (navRight.style.height === '0px' || navRight.style.height === '') {
+    const navLinksLenght = navLinks.length
+    const navLinkHeight = navLinks[0].clientHeight
+    const containerSize = navLinksLenght * navLinkHeight
+
+    /* SET THE HEIGHT OF THE MOBILE MENU */
+    navRight.style.height = containerSize.toString().concat('px')
+  }
+  else { navRight.style.height = '0px'; }
 }
 
+/* SET FOOTER COPYRIGHT YEAR */
+footerYear = document.querySelector('.copyright-year')
+const year = new Date().getFullYear()
+footerYear.innerHTML = year
 
