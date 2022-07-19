0d8738aca507faa98c8f26f85619042939aa0e56
exd02 <eliamxd1337@gmail.com>
2022-07-19 13:18:26 -0300

    Fixed navbar on scroll

--- 10-Scroll/main.js
+++ 10-Scroll/main.js
@@ -1,6 +1,7 @@
 
 const navLinks = document.querySelectorAll('.nav-links')
-navRight = document.querySelector('.nav-right')
+const navRight = document.querySelector('.nav-right')
+const navbar = document.getElementById('navbar')
 
 console.log (navRight.style.height)
 
@@ -8,18 +9,29 @@ console.log (navRight.style.height)
 /* - MOBILE NAVIGATION - */
 function toggleMenuView() {
   if (navRight.style.height === '0px' || navRight.style.height === '') {
-    const navLinksLenght = navLinks.length
-    const navLinkHeight = navLinks[0].clientHeight
-    const containerSize = navLinksLenght * navLinkHeight
+    let containerSize = 0
+    /* LOOP THROUGHT ALL */
+    navLinks.forEach (element => {
+      containerSize += element.clientHeight
+    });
 
-    /* SET THE HEIGHT OF THE MOBILE MENU */
-    navRight.style.height = containerSize.toString().concat('px')
+    navRight.style.height = `${containerSize}.px`
   }
   else { navRight.style.height = '0px'; }
 }
+
+
+/* FIXED NAVBAR ON SCROLL */
+window.addEventListener("scroll", function(){
+  const scrollHeight = window.pageYOffset
+  const navbarHeight = navbar.clientHeight
+  if (scrollHeight >= navbarHeight)
+    navbar.classList.add('fixed-nav')
+  else 
+    navbar.classList.remove('fixed-nav')
+})
 
 /* SET FOOTER COPYRIGHT YEAR */
 footerYear = document.querySelector('.copyright-year')
 const year = new Date().getFullYear()
 footerYear.innerHTML = year
-
