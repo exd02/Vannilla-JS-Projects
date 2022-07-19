aaf051b1740e0e1e3e7ee304ca7e08b2c6df268d
exd02 <eliamxd1337@gmail.com>
2022-07-19 15:14:02 -0300

    fixed dynamic scroll

--- 10-Scroll/main.js
+++ 10-Scroll/main.js
@@ -1,10 +1,12 @@
 
-const navLinks = document.querySelectorAll('.nav-links')
+const navLinks = document.querySelectorAll('.section-navigation')
+const sectionNavigationLinks = document.querySelectorAll('.section-navigation')
 const navRight = document.querySelector('.nav-right')
+const navHeader = document.querySelector('.nav-header')
 const navbar = document.getElementById('navbar')
 
-console.log (navRight.style.height)
 
+const sections = document.querySelectorAll('.section')
 
 /* - MOBILE NAVIGATION - */
 function toggleMenuView() {
@@ -17,7 +19,7 @@ function toggleMenuView() {
 
     navRight.style.height = `${containerSize}.px`
   }
-  else { navRight.style.height = '0px'; }
+  else { navRight.style.height = ''; }
 }
 
 
@@ -25,12 +27,34 @@ function toggleMenuView() {
 window.addEventListener("scroll", function(){
   const scrollHeight = window.pageYOffset
   const navbarHeight = navbar.clientHeight
+
   if (scrollHeight >= navbarHeight)
     navbar.classList.add('fixed-nav')
   else 
     navbar.classList.remove('fixed-nav')
 })
 
+/* FIX SCROLL THROUGHT WEBSITE */
+sectionNavigationLinks.forEach(link => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault()
+
+    // only toggle the menu view if it's opened
+    if (navRight.style.height != '')
+      toggleMenuView()
+
+    // get the href atribute and remove the '#'
+    let href = link.getAttribute("href").slice(1)
+
+    // get the refered section Y start offset
+    const referedSection_Y = document.getElementById(href).offsetTop
+
+    const navbarHeight = navHeader.clientHeight
+
+    // console.log(referedSection_Y + ' - ' + navbarHeight + ' = ' + (referedSection_Y - navbarHeight))
+    scroll(0, (referedSection_Y - navbarHeight))
+  })
+});
 /* SET FOOTER COPYRIGHT YEAR */
 footerYear = document.querySelector('.copyright-year')
 const year = new Date().getFullYear()
